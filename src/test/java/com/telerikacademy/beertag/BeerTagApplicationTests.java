a15ae8eb1034081841e29bd13a269fca8f6fd0e7
Krasen <krasenvachev3@gmail.com>
2019-06-14 13:05:36 +0300

    initial commit

---
+++ src/test/java/com/telerikacademy/beertag/BeerTagApplicationTests.java
@@ -0,0 +1,16 @@
+package com.telerikacademy.beertag;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BeerTagApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
