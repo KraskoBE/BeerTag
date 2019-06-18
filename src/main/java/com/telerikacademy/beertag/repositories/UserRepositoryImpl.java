5592464d17faecc31c7a2a357bafb76bb3894bfa
BaHeTo0 <baheto0@abv.bg>
2019-06-18 15:19:16 +0300

    User now has working update and delete/remove methods

--- src/main/java/com/telerikacademy/beertag/repositories/UserRepositoryImpl.java
+++ src/main/java/com/telerikacademy/beertag/repositories/UserRepositoryImpl.java
@@ -1,48 +1,64 @@
 package com.telerikacademy.beertag.repositories;
 
 import com.telerikacademy.beertag.models.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @org.springframework.stereotype.Repository("UserRepository")
 public class UserRepositoryImpl implements Repository<User> {
+    private SessionFactory sessionFactory;
     private List<User> users;
 
-    public UserRepositoryImpl() {
+    @Autowired
+    public UserRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
         users = new ArrayList<>();
     }
 
     @Override
     public User add(User user) {
-        users.add(user);
+        Session session = sessionFactory.getCurrentSession();
+        session.save(user);
         return user;
     }
 
     @Override
     public User get(int id) {
-        return users.stream()
-                .filter(x -> x.getId() == id)
-                .findFirst()
-                .orElseThrow(() -> new RuntimeException(String.format("User with the id of %d does not exist", id)));
+        Session session = sessionFactory.getCurrentSession();
+        return session.get(User.class, id);
     }
 
     @Override
     public User update(User oldUser, User newUser) {
+        Session session = sessionFactory.getCurrentSession();
+        Transaction transaction = session.beginTransaction();
         oldUser.setAge(newUser.getAge());
         oldUser.setEmail(newUser.getEmail());
         oldUser.setName(newUser.getName());
         oldUser.setPassword(oldUser.getPassword());
+        session.update(oldUser);
+        transaction.commit();
         return oldUser;
     }
 
     @Override
     public void remove(User user) {
-        users.remove(user);
+        Session session = sessionFactory.getCurrentSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(user);
+        transaction.commit();
     }
 
     @Override
     public List<User> getAll() {
-        return users;
+        Session session = sessionFactory.getCurrentSession();
+        Query<User> query = session.createQuery("FROM User", User.class);
+        return query.list();
     }
 }
