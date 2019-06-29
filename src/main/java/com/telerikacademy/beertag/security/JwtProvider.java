cda25428cf36b6a29bcaf9596e2dfc07fe4088a4
BaHeTo0 <baheto0@abv.bg>
2019-06-29 12:22:35 +0300

    Created some of the security files, commiting so i can work on  it from the PC

---
+++ src/main/java/com/telerikacademy/beertag/security/JwtProvider.java
@@ -0,0 +1,99 @@
+package com.telerikacademy.beertag.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtProvider {
+
+    //TODO find the bad practice here and fix it (:
+
+    @Value("${security.jwt.secret-key:secret-key}")
+    private String secretKey;
+
+    @Value("${security.jwt.expire-length:3600000}")
+    private long validityInMilliseconds = 3600000; // = 01:00:00
+
+    @Autowired
+    private UserAuth userAuth;
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+
+    //TODO import UserRole
+    public String createToken(String username, List<UserRole> roles) {
+
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put("auth",
+                roles.stream().map(s -> new SimpleGrantedAuthority(s.getAuthority()))
+                        .filter(Object::nonNull)
+                        .collect(Collectors.toList()));
+
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = userAuth.loadUserByUsername(getUsername(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException ex) {
+            throw new CustomException("Expired or invalid JWT token", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
