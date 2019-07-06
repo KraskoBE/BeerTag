973f9bd1791d621d71abac31e2d0cb2214323e1b
BaHeTo0 <BaHeTo0@abv.bg>
2019-07-07 00:18:14 +0300

    oops i missed the controller

--- src/main/java/com/telerikacademy/beertag/controllers/TagController.java
+++ src/main/java/com/telerikacademy/beertag/controllers/TagController.java
@@ -1,4 +1,34 @@
 package com.telerikacademy.beertag.controllers;
 
+import com.telerikacademy.beertag.models.Tag;
+import com.telerikacademy.beertag.services.TagService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/tags")
 public class TagController {
+
+    private final TagService tagService;
+
+    @Autowired
+    public TagController(TagService tagService) {
+        this.tagService = tagService;
+    }
+
+    @GetMapping
+    public List<Tag> findAll() {
+        return tagService.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Tag> save(@RequestBody final Tag tag) {
+        return tagService.save(tag)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.badRequest().build());
+    }
 }
